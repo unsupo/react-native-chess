0a338a508378e57f5732942dfabd59bd698f94a4
Jonathan Arndt <jarndt@salesforce.com>
2022-07-07 14:37:20 -0600

    Thu

--- App.js
+++ App.js
@@ -65,7 +65,6 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
