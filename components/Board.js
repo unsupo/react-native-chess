b7813c8b36c6f76e786fd710e586ee2848bc4ca3
Jonathan Arndt <jarndt@salesforce.com>
2022-07-08 11:37:18 -0600

    Initial commit

--- components/Board.js
+++ components/Board.js
@@ -25,22 +25,10 @@ const Board = () => {
 };
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
 });
 
 export default Board;
