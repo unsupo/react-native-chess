20ac9c7cdb9cd51dfbe1257503347bdb34a4d83a
Jonathan Arndt <jarndt@salesforce.com>
2022-07-08 12:34:48 -0600

    Fri

--- App.js
+++ App.js
@@ -35,6 +35,8 @@ const App: () => Node = () => {
 
 const styles = StyleSheet.create({
     app: {
+        width: '100%',
+        height: '100%',
         backgroundColor: '#33111e',
         justifyContent: 'center',
         alignItems: 'center'
