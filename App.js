5752a3ea162d72b5910891196f4ff5ee0264ef82
Jonathan Arndt <jarndt@salesforce.com>
2022-07-08 12:21:14 -0600

    Fri

--- App.js
+++ App.js
@@ -28,6 +28,10 @@ import Board from "./components/Board";
 const App: () => Node = () => {
     const isDarkMode = useColorScheme() === 'dark';
 
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
     return (
         <SafeAreaView style={backgroundStyle}>
             <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
