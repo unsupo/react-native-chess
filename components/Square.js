2738f48ebbf6a63d7c073a6265093fd03b0815dd
Jonathan Arndt <jarndt@salesforce.com>
2022-07-08 11:30:52 -0600

    Initial commit

--- components/Square.js
+++ components/Square.js
@@ -21,22 +21,6 @@ const Square = () => {
 
 const styles = StyleSheet.create({
     backgroundColor: 'blue',
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
 });
 
 export default Square;
