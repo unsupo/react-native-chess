df712559b7831cf8644fdc6319d434a00c2b8c45
Jonathan Arndt <jarndt@salesforce.com>
2022-07-08 13:59:38 -0600

    Fri

--- components/Square.js
+++ components/Square.js
@@ -29,11 +29,12 @@ const pieces = [
     "wr.png",
 ]
 
+{/*{props.piece < 0 || !props.piece ? <View/> :*/}
+{/*<Image source={require('../pieces/' + pieces[props.piece])}/>}</View>*/}
 const Square = (props) => {
 
     return (
-        <View style={styles(props)}>{props.piece < 0 || !props.piece ? <View/> :
-            <Image source={require('../pieces/' + pieces[props.piece])}/>}</View>
+        <View style={styles(props)}><Text>{'../pieces/' + pieces[props.piece]}</Text></View>
     );
 };
 
