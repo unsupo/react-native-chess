33a92353ada2e2a7a774856595ea646b9a561d61
Jonathan Arndt <jarndt@salesforce.com>
2022-07-11 11:43:18 -0600

    Mon

--- pieces/Pieces.const.js
+++ pieces/Pieces.const.js
@@ -1,4 +1,7 @@
+function shift(number, shift) {
+    return number * Math.pow(2, shift);
+}
 const BIT_INIT_POSITIONS = {
     "KING_W" : 1 << 3,
-    "KING_B" : 1 << (8 * 7 + 3),
+    "KING_B" : 1 << shift(8*7+3),
 }
