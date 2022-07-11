132c66ad04e1e872b692aba6e8d5fa01a6e7a7d7
Jonathan Arndt <jarndt@salesforce.com>
2022-07-11 13:20:11 -0600

    Mon

--- components/Board.js
+++ components/Board.js
@@ -53,6 +53,9 @@ const convert = (board) => {
     });
     return nBoard;
 }
+const convertCord = (x,y) => {
+
+}
 
 const Board = () => {
     const chess = new Chess()
@@ -71,7 +74,7 @@ const Board = () => {
                 <View style={styles.row} key={i}>
                     {[...Array(h)].map((y, j) =>
                         <TouchableHighlight key={i + ',' + j+ "touch"}
-                            onPress={()=>console.log(chess.moves())}>
+                            onPress={()=>console.log(i+','+j)}>
                             <Square piece={board[j][i]} key={i + ',' + j} size={size / w}
                                     color={(i + j) % 2 === 0 ? '#d0c1a9' : '#346e37'}/>
                         </TouchableHighlight>
