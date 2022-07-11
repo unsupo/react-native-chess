7bcad143df2747219ba7511f1295ba9e5f859edf
Jonathan Arndt <jarndt@salesforce.com>
2022-07-11 14:08:12 -0600

    Initial commit

--- components/Square.js
+++ components/Square.js
@@ -52,7 +52,7 @@ const PossibleTakeMove = () => {
             <Defs>
                 <Mask id="hole">
                     <Rect width="100%" height="100%" fill="white"/>
-                    <Circle r="50" cx="100" cy="100" fill="black"/>
+                    <Circle r="5%" cx="50%" cy="50%" fill="black"/>
                 </Mask>
             </Defs>
             <Circle id="donut" r="50%" cx="50%" cy="50%" fill="black" fillOpacity=".1" mask="url(#hole)" />
