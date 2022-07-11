47bd4e508a201b2b0a674a2a9b838498f5c54390
Jonathan Arndt <jarndt@salesforce.com>
2022-07-11 14:35:34 -0600

    Initial commit

--- components/Board.js
+++ components/Board.js
@@ -81,7 +81,7 @@ const Board = () => {
                                 if(pressed && (pressed['to'] || pressed['take']) &&
                                     pressed['to'].concat(pressed['take']).indexOf(coords) >= 0) {
                                     chess.move({from: pressed.from, to: coords});
-                                    return setBoard(convert(chess.board));
+                                    return setBoard(convert(chess.board()));
                                 }
                                 const obj = {};
                                 const moves = chess.moves({verbose: true, square: coords});
