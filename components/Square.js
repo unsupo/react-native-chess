806e199895242817cfb36de320bac0e17fc164c1
Jonathan Arndt <jarndt@salesforce.com>
2022-07-11 13:55:10 -0600

    Mon

--- components/Square.js
+++ components/Square.js
@@ -34,7 +34,7 @@ const Square = (props) => {
         <ImageBackground source={pieces[props.piece]} style={styles(props)}>
             {props.coord[0] === "a" || props.coord[1] === "1" ? <Text>{props.coord}</Text> : ""}
             <Svg height="100%" width="100%">
-                <Circle r="15%" cx="50%" cy="50%" fill="grey" fillOpacity=".4"  />
+                <Circle r="17%" cx="50%" cy="50%" fill="black" fillOpacity=".1"  />
             </Svg>
             {/*<Image style={styles(props)} source={pieces[props.piece]}/>*/}
         </ImageBackground>
