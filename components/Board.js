bf97013671888914d19206bafe2f3e181ba3d18f
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 12:44:16 -0600

    Tue Jul 12 18:44:16 2022|/Users/jarndt/code_projects/chess/components/Board.js|PlatformSpecific Updated IsFile

--- components/Board.js
+++ components/Board.js
@@ -165,7 +165,7 @@ const Board = () => {
                                     take={pressed['take']}
                                     to={pressed['to']}
                                     from={pressed['from']}
-                                    history={chess.history({verbose: true})[-1]}
+                                    history={chess.history({verbose: true})}
                                     color={(i + j) % 2}/>
                         </TouchableHighlight>
                     )}
