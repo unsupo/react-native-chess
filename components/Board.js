ad2f80565c90a153fda6013ed51ff6d085560e51
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 16:43:07 -0600

    Tue

--- components/Board.js
+++ components/Board.js
@@ -109,10 +109,11 @@ const Board = () => {
             }else
                 chess.move({from: pressed.from, to: coords});
             // console.log({from: pressed.from, to: coords, p: p})
-            setBoard(convert(chess.board()));
             getBestMove(chess.fen()).then(value => {
-                chess.move(from: value.slice(0,2), to: value.slice(2,4))
+                chess.move({from: value.slice(0,2), to: value.slice(2,4)});
+
             })
+            setBoard(convert(chess.board()));
             return setPressed({});
         }
         const obj = {};
