077e0b02ac58b98fb1ad6441bcd5f8d59f8b4582
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 11:28:24 -0600

    Tue Jul 12 17:28:24 2022|/Users/jarndt/code_projects/chess/components/Board.js|PlatformSpecific Updated IsFile

--- components/Board.js
+++ components/Board.js
@@ -193,7 +193,6 @@ const styles = (props) => StyleSheet.create({
 
 const modalStyles = StyleSheet.create({
     centeredView: {
-        width: '15%',
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
