d073426613e7a80290ac2f4fad79d7c84035ffc7
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 12:33:31 -0600

    Tue Jul 12 18:33:31 2022|/Users/jarndt/code_projects/chess/components/Board.js|PlatformSpecific Updated IsFile

--- components/Board.js
+++ components/Board.js
@@ -76,7 +76,7 @@ const Board = () => {
     useEffect(() => setBoard(convert(chess.board())), []);
 
     function squarePressed(i, j, p) {
-        console.log(chess.history());
+        console.log(chess.history({verbose: true}));
         const coords = convertCord(i, j);
         if (pressed && (pressed['to'] || pressed['take']) &&
             pressed['to'].concat(pressed['take']).indexOf(coords) >= 0) {
