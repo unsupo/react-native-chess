236088ec2922a5de08a8f30d4f5222a08a5d2a88
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 15:23:07 -0600

    Tue

--- components/ai.js
+++ components/ai.js
@@ -3,18 +3,26 @@ import { mainLoop, shutdownStockfish, sendCommand } from 'react-native-stockfish
 import { NativeEventEmitter, NativeModules } from 'react-native'; // in order to read Stockfish output.
 
 
+class AI {
+    eventEmitter;
 
-// In startup hook
-const eventEmitter = new NativeEventEmitter(NativeModules.ReactNativeStockfishChessEngine);
-// Also you need to listen to the event 'stockfish-output' in order to get output lines from Stockfish.
-const eventListener = eventEmitter.addListener('stockfish-output', (line) => {
-    console.log("Stockfish output: "+line);
-});
-await mainLoop(); // starts the engine process.
+    async constructor() {
+        eventListener = new NativeEventEmitter(NativeModules.ReactNativeStockfishChessEngine);
+        // In startup hook
+        // Also you need to listen to the event 'stockfish-output' in order to get output lines from Stockfish.
+        const eventListener = eventEmitter.addListener('stockfish-output', (line) => {
+            console.log("Stockfish output: " + line);
+        });
+        await mainLoop(); // starts the engine process.
+    }
+
+    async stop() {
+        // In will destroy hook
+        await shutdownStockfish(); // dispose the engine process
+        eventListener.remove(); // dispose the Stockfish output reader process.
+    }
+}
+
 
 // When you need to send a command (e.g) : don't forget the newline character :
 await sendCommand("position start\n");
-
-// In will destroy hook
-await shutdownStockfish(); // dispose the engine process
-eventListener.remove(); // dispose the Stockfish output reader process.
