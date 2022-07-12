0d0f05a82d7352f665b88b178f4433827de271e4
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 15:24:24 -0600

    Tue

--- components/ai.js
+++ components/ai.js
@@ -4,22 +4,23 @@ import { NativeEventEmitter, NativeModules } from 'react-native'; // in order to
 
 
 class AI {
-    eventEmitter;
+    // In startup hook
+    eventEmitter = new NativeEventEmitter(NativeModules.ReactNativeStockfishChessEngine);
+    // Also you need to listen to the event 'stockfish-output' in order to get output lines from Stockfish.
+    eventListener = this.eventEmitter.addListener('stockfish-output', (line) => {
+        console.log("Stockfish output: " + line);
+    });
 
     async constructor() {
-        eventListener = new NativeEventEmitter(NativeModules.ReactNativeStockfishChessEngine);
-        // In startup hook
-        // Also you need to listen to the event 'stockfish-output' in order to get output lines from Stockfish.
-        const eventListener = eventEmitter.addListener('stockfish-output', (line) => {
-            console.log("Stockfish output: " + line);
-        });
         await mainLoop(); // starts the engine process.
     }
+
+    
 
     async stop() {
         // In will destroy hook
         await shutdownStockfish(); // dispose the engine process
-        eventListener.remove(); // dispose the Stockfish output reader process.
+        this.eventListener.remove(); // dispose the Stockfish output reader process.
     }
 }
 
