b24a8075cdeec5b87268e09c9a0120e1f11e338c
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 12:47:17 -0600

    Tue Jul 12 18:47:17 2022|/Users/jarndt/code_projects/chess/components/Square.js|PlatformSpecific Updated IsFile

--- components/Square.js
+++ components/Square.js
@@ -55,7 +55,6 @@ const colors = {
     BH: '#BACA2B'
 }
 const getColor = (props) =>{
-    console.log(props.history)
     let history = props.history === undefined || props.history.length === 0 ? {from: "nullC", to: "nullC"} : props.history[-1];
     let c = colors.A; let ch = colors.AH;
     if(props.color === 1){
