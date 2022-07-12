daad48227b61699139fd410d6f72b5d34d23f1bb
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 15:55:09 -0600

    Tue Jul 12 20:23:06 2022|/Users/jarndt/code_projects/chess/android/app/build/intermediates/incremental/mergeDebugResources/merged.dir/values-am/values-am.xml|Created Updated IsFile

--- components/Board.js
+++ components/Board.js
@@ -86,9 +86,9 @@ const Board = () => {
                 console.log("Stockfish output: "+line);
             });
             await mainLoop()
-            await sendCommand("ucinewgame\n");
-            await sendCommand("position fen " + chess.fen() + "\n")
-            await sendCommand("go movetime 1000\n");
+            // await sendCommand("ucinewgame\n");
+            // await sendCommand("position fen " + chess.fen() + "\n")
+            // await sendCommand("go movetime 1000\n");
         } // starts the engine process.
         asyncMainLoop()
     }, []);
