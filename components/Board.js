28a17fad3afcb476776f16e6638bf57e4bf9887d
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 10:59:54 -0600

    Tue Jul 12 16:59:54 2022|/Users/jarndt/code_projects/chess/components/Board.js|PlatformSpecific Updated IsFile

--- components/Board.js
+++ components/Board.js
@@ -130,20 +130,17 @@ const Board = () => {
                     setModalVisible(!modalVisible);
                 }}
             >
-                <TouchableWithoutFeedback onPress={() => {
-                }}>
-                    <View style={modalStyles.centeredView}>
-                        <View style={modalStyles.modalView}>
-                            <Text style={modalStyles.modalText}>Hello World!</Text>
-                            <Pressable
-                                style={[modalStyles.button, modalStyles.buttonClose]}
-                                onPress={() => setModalVisible(!modalVisible)}
-                            >
-                                <Text style={modalStyles.textStyle}>Hide Modal</Text>
-                            </Pressable>
-                        </View>
+                <View style={modalStyles.centeredView}>
+                    <View style={modalStyles.modalView}>
+                        <Text style={modalStyles.modalText}>Hello World!</Text>
+                        <Pressable
+                            style={[modalStyles.button, modalStyles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={modalStyles.textStyle}>Hide Modal</Text>
+                        </Pressable>
                     </View>
-                </TouchableWithoutFeedback>
+                </View>
             </Modal>
         </TouchableWithoutFeedback>);
     }
