b2f8f345f86e2575497358a3c029e6eb5919a779
Jonathan Arndt <jarndt@salesforce.com>
2022-07-12 16:29:09 -0600

    Initial commit

--- components/Board.js
+++ components/Board.js
@@ -77,7 +77,7 @@ const Board = () => {
     }, []);
 
     async function getBestMove(fen) {
-        const response = await fetch('http://localhost:8080', {method: 'POST', body: JSON.stringify({'fen': fen})}).catch(reason => console.log("request error: "+reason));
+        const response = await fetch('http://169.254.172.225:8080', {method: 'POST', body: JSON.stringify({'fen': fen})}).catch(reason => console.log("request error: "+reason));
         return response ? response.json() : "";
     }
 
